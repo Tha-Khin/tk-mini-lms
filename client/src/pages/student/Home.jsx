1ef841e61a927649c95d611bad98b8d644071d6d
Tha_Khin <thakhin.pp@gmail.com>
2025-10-23 18:54:19 +0700

    first commit

---
+++ client/src/pages/student/Home.jsx
@@ -0,0 +1,11 @@
+import CoursesSection from '../../components/student/CoursesSection'
+
+const Home = () => {
+  return (
+    <div className='flex flex-col items-center space-y-7 text-center'>
+      <CoursesSection/>
+    </div>
+  )
+}
+
+export default Home

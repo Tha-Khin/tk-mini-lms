4ff97671c67dfa7067c0d2a86f4e358e73019740
Tha_Khin <thakhin.pp@gmail.com>
2025-10-31 19:58:51 +0700

    courses - grid col 1 in mobile

--- client/src/components/CoursesSection.jsx
+++ client/src/components/CoursesSection.jsx
@@ -9,7 +9,7 @@ const CoursesSection = () => {
     <div className='py-16 px-8 md:px-40 bg-linear-to-b from-cyan-100/70 w-full'>
       <h2 className='text-3xl font-medium text-gray-800'>Learn from the best</h2>
       <p className='text-sm md:text-base text-gray-500 mt-3'>Discover our top-rated courses across various categories. From coding and design to <br/> business and wellness, our courses are crafted to deliver results.</p>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 px-4 md:px-0 md:my-16 my-10 gap-4'>
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 px-4 md:px-0 md:my-16 my-10 gap-4'>
         {allCourses.slice(0,5).map((course, index)=> <CourseCard key={index} course={course}/>)}
       </div>
     </div>
